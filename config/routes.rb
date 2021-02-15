8e7bcf9bacc1ae84aba3a8c6b6adc4b6d8009f34
skrinits <110100reg@gmail.com>
2021-02-15 21:43:40 +0300

    admin authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'home#index'
+
+  devise_for :admins
+
   namespace :api do
     namespace :v1 do
       resources :transactions, only: :create
---
+++ spec/support/devise.rb
@@ -0,0 +1,13 @@
+module ControllerMacros
+  def login_admin
+    before(:each) do
+      @request.env['devise.mapping'] = Devise.mappings[:admin]
+      sign_in FactoryBot.create(:admin)
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.extend ControllerMacros, type: :controller
+end
